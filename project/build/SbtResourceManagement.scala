51c07550ab325706562be64da6fbb56bfd7b70db
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2012-02-03 16:06:24 -0500

    Drop lift dependency.
    We can't use it with Scala 2.7.7, which is what sbt 0.7.4 runs on.

--- project/build/SbtResourceManagement.scala
+++ project/build/SbtResourceManagement.scala
@@ -1,9 +1,7 @@
 import sbt._
 
 class SbtResourceManagementPluginProject(info:ProjectInfo) extends PluginProject(info) {
-  val liftVersion = "2.4"
   override def libraryDependencies = Set(
-    "net.liftweb" %% "lift-util" % liftVersion % "compile->default",
     "net.java.dev.jets3t" % "jets3t" % "0.8.1",
     "com.yahoo.platform.yui" % "yuicompressor" % "2.4.2"
   ) ++ super.libraryDependencies
