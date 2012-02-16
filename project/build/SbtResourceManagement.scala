c4fe72d71ea2638b72e718ad89f5c619df64c4b4
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2012-02-16 15:56:00 -0500

    Switch to included yuicompressor JAR.
    Pulling it in via sbt pulls in a Rhino dependency that busts things up
    something rotten.

--- project/build/SbtResourceManagement.scala
+++ project/build/SbtResourceManagement.scala
@@ -2,7 +2,6 @@ import sbt._
 
 class SbtResourceManagementPluginProject(info:ProjectInfo) extends PluginProject(info) {
   override def libraryDependencies = Set(
-    "net.java.dev.jets3t" % "jets3t" % "0.8.1",
-    "com.yahoo.platform.yui" % "yuicompressor" % "2.4.2"
+    "net.java.dev.jets3t" % "jets3t" % "0.8.1"
   ) ++ super.libraryDependencies
 }
