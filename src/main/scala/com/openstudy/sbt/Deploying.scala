cd13a4b51d7716617c203c28047d67bff9454d27
Matt Farmer <matt@frmr.me>
2014-02-23 00:35:29 -0500

    Split out Deploying trait.

---
+++ src/main/scala/com/openstudy/sbt/Deploying.scala
@@ -0,0 +1,78 @@
+package com.openstudy
+package sbt
+
+import java.io._
+
+import _root_.sbt.{File => SbtFile, _}
+import Keys.{baseDirectory, resourceDirectory, streams, target, _}
+
+trait Deploying {
+  val deployResources = TaskKey[Unit]("deploy-resources")
+
+  def customBucketMap: scala.collection.mutable.HashMap[String, List[String]]
+
+  def doDeploy(streams:TaskStreams, checksumInFilename:Boolean, bundleChecksums:Map[String,String], bundleVersions:File, baseCompressedTarget:File, files:Seq[File], mimeType:String, access:String, secret:String, bucket:String) = {
+    try {
+      val handler = new S3Handler(access, secret, bucket)
+
+      IO.write(bundleVersions.asFile, "checksum-in-filename=" + checksumInFilename + "\n")
+      for {
+        file <- files
+        bundle =
+          (if (checksumInFilename)
+            file.base.split("-").dropRight(1).mkString("-")
+          else
+            file.base)
+        checksum <- bundleChecksums.get(bundle)
+        relativePath <- IO.relativize(baseCompressedTarget, file)
+      } yield {
+        streams.log.info("  Deploying bundle " + bundle + " as " + relativePath + "...")
+
+        try {
+          val contents = IO.readBytes(file)
+          handler.saveFile(mimeType, relativePath, contents)
+
+          IO.append(bundleVersions, bundle + "=" + checksum + "\n")
+        } catch {
+          case e: Exception =>
+            streams.log.error(e.getMessage + "\n" + e.getStackTrace.mkString("\n"))
+            throw new RuntimeException("Failed to upload " + file)
+        }
+      }
+    } catch {
+      case exception: Exception =>
+        streams.log.error(exception.toString + "\n" + exception.getStackTrace.map(_.toString).mkString("\n"))
+        throw new RuntimeException("Deploy failed.")
+    }
+  }
+
+  protected def withAwsConfiguration(streams: TaskStreams, access: Option[String], secret: Option[String], defaultBucket: Option[String])(deployHandler: (String,String,String)=>Unit) = {
+    val abort_? = access.isEmpty || secret.isEmpty || defaultBucket.isEmpty
+    if (access.isEmpty)
+      streams.log.error("To use AWS deployment, you must set awsAccessKey := Some(\"your AWS access key\") in your sbt build.")
+    if (secret.isEmpty)
+      streams.log.error("To use AWS deployment, you must set awsSecretKey := Some(\"your AWS secret key\") in your sbt build.")
+    if (defaultBucket.isEmpty)
+      streams.log.error("To use AWS deployment, you must set awsS3Bucket := Some(\"your S3 bucket name\") in your sbt build.")
+
+    if (abort_?)
+      throw new RuntimeException("Missing AWS info, aborting deploy. See previous errors for more information.")
+    else
+      deployHandler(access.get, secret.get, defaultBucket.get)
+  }
+
+  protected def withBucketMapping(bundles:Seq[File], defaultBucket:String, customBucketMap:scala.collection.Map[String, List[String]])(deployHandler:(String, Seq[File])=>Unit) = {
+    val bundlesForDefaultBucket = bundles.filterNot { (file) =>
+      customBucketMap.exists { case (id, files) => files.contains(file.getName) }
+    }
+    deployHandler(defaultBucket, bundlesForDefaultBucket)
+
+    for {
+      customBucketName <- customBucketMap.keys
+      bucketFiles <- customBucketMap.get(customBucketName)
+      bundlesForBucket = bundles.filter(file => bucketFiles.contains(file.getName))
+    } {
+      deployHandler(customBucketName, bundlesForBucket)
+    }
+  }
+}
--- src/main/scala/com/openstudy/sbt/ResourceManagementPlugin.scala
+++ src/main/scala/com/openstudy/sbt/ResourceManagementPlugin.scala
@@ -5,9 +5,7 @@ package com.openstudy { package sbt {
   import _root_.sbt.{File => SbtFile, _}
   import Keys.{baseDirectory, resourceDirectory, streams, target, _}
 
-  import com.yahoo.platform.yui.compressor._
-
-  object ResourceManagementPlugin extends Plugin with SassCompiling with LessCompiling with CoffeeScriptCompiling with ScriptCompressing with CssCompressing {
+  object ResourceManagementPlugin extends Plugin with SassCompiling with LessCompiling with CoffeeScriptCompiling with ScriptCompressing with CssCompressing with Deploying {
     val ResourceCompile = config("resources")
 
     private val webappResourceAlias = SettingKey[Seq[File]]("webapp-resources")
@@ -29,7 +27,6 @@ package com.openstudy { package sbt {
     val copyScripts = TaskKey[Unit]("copy-scripts")
     val deployScripts = TaskKey[Unit]("deploy-scripts")
     val deployCss = TaskKey[Unit]("deploy-styles")
-    val deployResources = TaskKey[Unit]("deploy-resources")
 
     val customBucketMap = scala.collection.mutable.HashMap[String, List[String]]()
 
@@ -55,68 +52,6 @@ package com.openstudy { package sbt {
       IO.copy(scriptCopyPaths, true)
     }
 
-    def doDeploy(streams:TaskStreams, checksumInFilename:Boolean, bundleChecksums:Map[String,String], bundleVersions:File, baseCompressedTarget:File, files:Seq[File], mimeType:String, access:String, secret:String, bucket:String) = {
-      try {
-        val handler = new S3Handler(access, secret, bucket)
-
-        IO.write(bundleVersions.asFile, "checksum-in-filename=" + checksumInFilename + "\n")
-        for {
-          file <- files
-          bundle =
-            (if (checksumInFilename)
-              file.base.split("-").dropRight(1).mkString("-")
-            else
-              file.base)
-          checksum <- bundleChecksums.get(bundle)
-          relativePath <- IO.relativize(baseCompressedTarget, file)
-        } yield {
-          streams.log.info("  Deploying bundle " + bundle + " as " + relativePath + "...")
-
-          try {
-            val contents = IO.readBytes(file)
-            handler.saveFile(mimeType, relativePath, contents)
-
-            IO.append(bundleVersions, bundle + "=" + checksum + "\n")
-          } catch {
-            case e: Exception =>
-              streams.log.error(e.getMessage + "\n" + e.getStackTrace.mkString("\n"))
-              throw new RuntimeException("Failed to upload " + file)
-          }
-        }
-      } catch {
-        case exception: Exception =>
-          streams.log.error(exception.toString + "\n" + exception.getStackTrace.map(_.toString).mkString("\n"))
-          throw new RuntimeException("Deploy failed.")
-      }
-    }
-    private def withAwsConfiguration(streams: TaskStreams, access: Option[String], secret: Option[String], defaultBucket: Option[String])(deployHandler: (String,String,String)=>Unit) = {
-      val abort_? = access.isEmpty || secret.isEmpty || defaultBucket.isEmpty
-      if (access.isEmpty)
-        streams.log.error("To use AWS deployment, you must set awsAccessKey := Some(\"your AWS access key\") in your sbt build.")
-      if (secret.isEmpty)
-        streams.log.error("To use AWS deployment, you must set awsSecretKey := Some(\"your AWS secret key\") in your sbt build.")
-      if (defaultBucket.isEmpty)
-        streams.log.error("To use AWS deployment, you must set awsS3Bucket := Some(\"your S3 bucket name\") in your sbt build.")
-
-      if (abort_?)
-        throw new RuntimeException("Missing AWS info, aborting deploy. See previous errors for more information.")
-      else
-        deployHandler(access.get, secret.get, defaultBucket.get)
-    }
-    def withBucketMapping(bundles:Seq[File], defaultBucket:String, customBucketMap:scala.collection.Map[String, List[String]])(deployHandler:(String, Seq[File])=>Unit) = {
-      val bundlesForDefaultBucket = bundles.filterNot { (file) =>
-        customBucketMap.exists { case (id, files) => files.contains(file.getName) }
-      }
-      deployHandler(defaultBucket, bundlesForDefaultBucket)
-
-      for {
-        customBucketName <- customBucketMap.keys
-        bucketFiles <- customBucketMap.get(customBucketName)
-        bundlesForBucket = bundles.filter(file => bucketFiles.contains(file.getName))
-      } {
-        deployHandler(customBucketName, bundlesForBucket)
-      }
-    }
     def doScriptDeploy(streams:TaskStreams, checksumInFilename:Boolean, bundleChecksums:Map[String,String], scriptBundleVersions:File, compressedTarget:File, access:Option[String], secret:Option[String], defaultBucket:Option[String]) = {
       val bundles = (compressedTarget / "javascripts" ** "*.js").get
 
