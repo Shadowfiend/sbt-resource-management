9387ae65018629379db13e3e75f96081be65f493
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2014-03-27 15:56:31 -0400

    Reindent S3Helpers and use better package syntax.

--- src/main/scala/com/openstudy/sbt/S3Helpers.scala
+++ src/main/scala/com/openstudy/sbt/S3Helpers.scala
@@ -1,40 +1,41 @@
-package com.openstudy { package sbt {
-  import java.io._
+package com.openstudy
+package sbt
 
-  import org.jets3t.service._
-    import acl._
-    import impl.rest.httpclient._
-    import model._
-    import security._
-    import utils._
+import java.io._
 
-  trait S3 {
-    def accessKey : String
-    def secretKey : String
+import org.jets3t.service._
+  import acl._
+  import impl.rest.httpclient._
+  import model._
+  import security._
+  import utils._
 
-    lazy val credentials =
-      new AWSCredentials(accessKey, secretKey)
-    lazy val s3 = new RestS3Service(credentials)
+trait S3 {
+  def accessKey : String
+  def secretKey : String
+
+  lazy val credentials =
+    new AWSCredentials(accessKey, secretKey)
+  lazy val s3 = new RestS3Service(credentials)
+}
+class S3Handler(val accessKey:String, val secretKey:String, bucket:String) extends S3 {
+  lazy val bucketAcl = {
+    val acl = s3.getBucketAcl(bucket);
+    acl.grantPermission(GroupGrantee.ALL_USERS, Permission.PERMISSION_READ);
+
+    acl
   }
-  class S3Handler(val accessKey:String, val secretKey:String, bucket:String) extends S3 {
-    lazy val bucketAcl = {
-      val acl = s3.getBucketAcl(bucket);
-      acl.grantPermission(GroupGrantee.ALL_USERS, Permission.PERMISSION_READ);
 
-      acl
-    }
+  // Returns the MD5 checksum of the file.
+  def saveFile(mime:String, fileName:String, data:Array[Byte]) : String = {
+    val s3Object = new S3Object(fileName, data)
+    val hash = s3Object.getMd5HashAsHex
 
-    // Returns the MD5 checksum of the file.
-    def saveFile(mime:String, fileName:String, data:Array[Byte]) : String = {
-      val s3Object = new S3Object(fileName, data)
-      val hash = s3Object.getMd5HashAsHex
+    s3Object.setContentType(mime)
+    s3Object.setAcl(bucketAcl)
 
-      s3Object.setContentType(mime)
-      s3Object.setAcl(bucketAcl)
+    s3.putObject(bucket, s3Object)
 
-      s3.putObject(bucket, s3Object)
-
-      hash
-    }
+    hash
   }
-} }
+}
